3a8284a0e333e33bfacfe0c372cd794c5967e754
Yunusbulbul <yblls655@gmail.com>
2026-03-28 23:30:57 +0300

    proje başladı

---
+++ android/app/src/main/kotlin/com/example/fatura_takip/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fatura_takip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold( 
+        appBar: AppBar(
+        title: Text("Fatura Takip"),
+       backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child:Column( 
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+              
+           child: CircleAvatar(
+          backgroundImage: NetworkImage("https://www.pngall.com/wp-content/uploads/5/Avatar-Facebook-PNG-High-Quality-Image.png"),
+          radius: 50,
+          )
+              ),
+          SizedBox(
+            height: 20, 
+          ),
+          Align(
+            alignment: Alignment.center, 
+            child: Text("Fatura Takip Uygulaması"),
+          ),
+            ],
+          
+        ),
+        )
+        )
+    );
+  }
+  }
